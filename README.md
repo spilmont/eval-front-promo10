658132edb96ff529657351ee6af123534628dd29
spilmont <43751712+spilmont@users.noreply.github.com>
2018-12-14 11:31:15 +0100

    essais smartphone

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 ## Evaluation Front
-Vous devez recréer un site statique, on vous demandera de le recréer. Ne vous inquiétez pas, mais l'objectif est de construire quelque chose qui respecte l'intégrité de la conception originale. il est nécessaire de créer ce qui reflètent le choix du "client" en matière de mise en page, d'espacement, de police, d'image et de couleur dans les moindres détails.
+Vous devez recréer un site statique,
+ on vous demandera de le recréer. Ne vous inquiétez pas,
+  mais l'objectif est de construire quelque chose qui respecte l'intégrité de la conception originale.
+   il est nécessaire de créer ce qui reflètent le choix du "client" en matière de mise en page,
+    d'espacement, de police, d'image et de couleur dans les moindres détails.
 
 Nous ne vous avons pas fourni de fichiers natifs. Vous devrez vous procurer les ressources par vos propres moyens.
 
-Vous devrez faire preuve d'un peu de créativité et d'une bonne dose de jugement. Pensez à la meilleure façon dont vous, en tant qu'utilisateur, pourriez faire l'expérience du contenu/de la mise en page à travers différentes tailles d'écran. Le contenu est-il lisible ? Est-ce que l'ergonomie (en particulier les images d'arrière-plan) a toujours un sens et une belle apparence ? Les parties de la navigation deviennent-elles des icônes (et sont-elles intuitives ?), ou un menu ?
+Vous devrez faire preuve d'un peu de créativité et d'une bonne dose de jugement.
+ Pensez à la meilleure façon dont vous, en tant qu'utilisateur,
+  pourriez faire l'expérience du contenu/de la mise en page à travers différentes tailles d'écran. 
+  Le contenu est-il lisible ? Est-ce que l'ergonomie (en particulier les images d'arrière-plan) a toujours un sens et une belle
+   apparence ? Les parties de la navigation deviennent-elles des icônes (et sont-elles intuitives ?), ou un menu ?
 
 #### Ressources Disponibles :
 
---
+++ script.js
@@ -0,0 +1,248 @@
+var carousel= ["img/1.jpg","img/2.jpg","img/3.jpg"];
+
+var index=0;
+var click =0;
+
+$('.carousel').css("background-image", "url("+carousel[0]+")");
+
+$('.D').click(function () {
+
+    if(index <2){
+
+       index++;
+        $('.carousel').css("background-image", "url("+carousel[index]+")");
+    }else{
+        index=0;
+        $('.carousel').css("background-image", "url("+carousel[0]+")");
+
+    }
+    console.log(index);
+});
+
+$('.G').click(function () {
+
+    if(index >0){
+
+        index--;
+        $('.carousel').css("background-image", "url("+carousel[index]+")");
+    }else{
+        index=carousel.length-1;
+        $('.carousel').css("background-image", "url("+carousel[2]+")");
+    }
+    console.log(index);
+});
+
+
+function suivant(){
+
+
+    if(index <2){
+
+        index++;
+        $('.carousel').css("background-image", "url("+carousel[index]+")");
+
+    }else{
+        index=0;
+        $('.carousel').css("background-image", "url("+carousel[0]+")");
+
+    }
+    setTimeout(suivant,2000);
+}
+suivant();
+
+$('#name').click(function () {
+
+    if(click==0){
+        click++;
+    $(this).animate({
+        width:'600px',
+        height: '600px',
+    });
+    $(".categorie").hide();
+}else {
+        click=0;
+        $(this).animate({
+            width: '400px',
+            height: '200px',
+        });
+        $(".categorie").show();
+    }
+
+});
+
+
+
+
+$('.experience').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+$('.education').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+
+$('.blog').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+$('.social').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+$('.profil').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+        $(this).html('Developpeur Web Junior');
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(this).html('<div class="categorie profil orange"> <span class="far fa-address-card"></span>Profil </div>');
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+$('.contact').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+$('.portfolio').click(function () {
+
+    if(click==0){
+        click++;
+        $(this).animate({
+            width:'600px',
+            height: '600px',
+        });
+        $("#name").hide();
+        $(".categorie").hide();
+        $(this).show();
+
+    }else {
+        click=0;
+        $(this).animate({
+            width: '200px',
+            height: '200px',
+        });
+        $(".categorie").show();
+        $("#name").show();
+
+    }
+});
+
+
+
+
